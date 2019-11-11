1fef13eb8cb413f1bab4fb6601e7b42bd63b6673
Aubin LORIEUX <aubin.lorieux@gmail.com>
2019-11-11 18:50:02 +0100

    Feature: add i3 configs

--- ubuntu/symlink.sh
+++ ubuntu/symlink.sh
@@ -15,3 +15,7 @@ ln -fs ${DOTFILES}/git/.gitignore ${HOME}/.gitignore
 # vim
 ln -fs ${DOTFILES}/vim/.vimrc.plug ${HOME}/.vimrc.plug
 ln -fs ${DOTFILES}/vim/.vimrc ${HOME}/.vimrc
+
+# i3
+ln -fs ${DOTFILES}/i3/config ${HOME}/.i3/config
+ln -fs ${DOTFILES}/i3/status.conf ${HOME}/.i3/status.conf
