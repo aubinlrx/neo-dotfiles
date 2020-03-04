34648d420bbd97a0230f760df9a2bfbc7ca11f8c
Aubin Lorieux <aubin.lorieux@gmail.com>
2020-03-04 11:36:20 +0100

    Tweak: improve zsh install

--- osx/install/zsh.sh
+++ osx/install/zsh.sh
@@ -4,3 +4,6 @@ brew install zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 upgrade_oh_my_zsh
 chsh -s /bin/zsh
+
+echo "install zsh-syntax-highlightin"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
